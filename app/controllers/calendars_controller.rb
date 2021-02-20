fad4dfdec8de496e53f69428ddc5383f562255c7
ponchanmasimasi <ponsan7.12@icloud.com>
2021-02-20 11:22:53 +0900

    Revert "Revert "Merge pull request #2 from ponchanmasimasi/second_training""
    This reverts commit b06e71eaaa3e0fa32b7816ff573bcf8ca0d1ef48.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -15,7 +16,8 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.require(:plan).permit(:date, :plan)
+    # :plan => {:data=>ssss, :plan => sss} 
   end
 
   def get_week
@@ -34,9 +36,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+
       days = { month : (@todays_date + x).month, date : (@todays_date+x).day, plans : (today_plans)}
+
       @week_days.push(days)
     end
-
+ 
   end
 end
